28df380c782250380b0c450f2aa89a071ad9937f
PotriRathna <88546064+PotriRathna@users.noreply.github.com>
2021-08-17 09:11:55 +0200

    Update TestMax_Difference.scala

--- src/test/scala/TestMax_Difference.scala
+++ src/test/scala/TestMax_Difference.scala
@@ -3,7 +3,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestMax_Difference extends AnyFunSuite{
   test("Max_Difference"){
-    assert(Max_Difference.difference(array1= Array(2, 3, 1, 7, 9, 5, 11, 3, 5),0,1,0)===0)
+    assert(Max_Difference.difference(array1= Array(2, 3, 1, 7, 9, 5, 11, 3, 5),0,1,0)===10)
   }
 
 }
