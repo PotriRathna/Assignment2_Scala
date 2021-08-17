bce26f633de9affcf816fea7438488a305602ca0
PotriRathna <88546064+PotriRathna@users.noreply.github.com>
2021-08-17 09:12:16 +0200

    Update TestMax_Product.scala

--- src/test/scala/TestMax_Product.scala
+++ src/test/scala/TestMax_Product.scala
@@ -3,7 +3,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestMax_Product extends AnyFunSuite{
   test("Max_product") {
-    assert(Max_Product.product(array1 = Array(2, 3, 1, 7, 9, 5, 11, 3, 5),0,1,0)===0)
+    assert(Max_Product.product(array1 = Array(2, 3, 1, 7, 9, 5, 11, 3, 5),0,1,0)===99)
   }
 
 }
